38718208cd24fa5122e1df7ab287a37d82c4d15e
Naim120 <sknaimuddin234@gmail.com>
2026-09-14 18:10:53 +0530

    Bump version to 1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sk.gallery"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val secretsFile = rootProject.file("secrets.properties")
