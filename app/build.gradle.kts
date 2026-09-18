ec70089f824948808c14114f6806c4d74c8796bd
Naim120 <sknaimuddin234@gmail.com>
2026-09-18 12:39:31 +0530

    Release v1.4.3: Refine Photos landing page padding to 2dp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sk.gallery"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.4.2"
+        versionCode = 10
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val secretsFile = rootProject.file("secrets.properties")
